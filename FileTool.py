0d84b4a92c396d74d3797ddb40cd4ef11ee5a695
Ilhan Mert <ilhanmertalan@gmail.com>
2021-12-29 02:25:53 +0300

    ADD opening file, reading methods

--- FileTool.py
+++ FileTool.py
@@ -1,1 +1,51 @@
+from io import TextIOWrapper
+from os import path
+class FileTool:
+    def __init__(self,path,fields):
+        self.path=path
+        self.fields=fields
+    
+    def openFile(self,appendMode=False):
+        """
+        FileTool Method
+        openFile(appendMode: bool) \n
+        When appendMode=True, file opens in a+ mode, 
+        appendMode=False, file opens in r+ mode.
+        """
+        mod=""
+        if path.exists(self.path):
+            mod="a+" if appendMode==True else "r+"
+        else:
+            mod="w+"
+        return open(self.path,mod)
 
+    def readAll(self, file :TextIOWrapper):
+        """
+        Takes "file" as argument:  \n
+        Arg. Type: TextIOWrapper \n
+        You can give returned value from openFile as an argument.
+        """
+        for row in file.readlines():
+            print(row)
+        print("\n")
+        file.seek(0)
+    
+    def readRange(self,file :TextIOWrapper,start :int,end :int):
+        """
+        First Argument is an opened file :TextIOWrapper\n
+        start: first row to read
+        end: last row to read
+        """
+        for row in file.readlines()[start:end+1]:
+            print(row)
+
+        
+        
+
+
+
+
+
+
+
+        
